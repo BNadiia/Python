7864d75a227c1c242cb104818ed465ae4716d666
Nadiia <sendahovm@gmail.com>
2018-10-21 10:42:13 +0300

    Update lab7_10.py
    add one more output

--- lab7_10.py
+++ lab7_10.py
@@ -7,11 +7,13 @@ def is_the_shortest(phrase):
     
     words = phrase.split()
     the_shortest = min(words, key = len)
-      
-    return the_shortest
+    len_of_the_shortest = len(the_shortest)
+     
+    return the_shortest, len_of_the_shortest
 
 
-def ui_output(the_shortest):
+def ui_output(the_shortest, len_of_the_shortest):
     print("The shortest word is: " + str(the_shortest))
+    print("Length of the shortest word is: " + str(len_of_the_shortest))
 
-ui_output(is_the_shortest(ui_input()))
+ui_output(*is_the_shortest(ui_input()))
