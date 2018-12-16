2933805ab6e72e80bf5b8018b03074378befa35c
Nadiia <sendahovm@gmail.com>
2018-12-16 16:14:36 +0200

    Update lab11_2.py

--- lab11_2.py
+++ lab11_2.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
-X = 0
+__seed = 0
 
-def fillZeros(s : str, size : int) -> str:
-  if len(s) >= size:
-    return s[:size]
-  for i in range(len(s),size):
-    s = "0" + s
-  return s
+def initGen(seed : int):
+  global __seed
+  __seed = seed
 
-def Rand(seed : int  = 0) ->int:
-  global X
-  if(seed != 0):
-    X = fillZeros(str(seed),6)
-  X = fillZeros(str(X), 6)
-  Y = int((X)[3:6] + (X)[:3])
-  Sum = fillZeros(str(int(X) * int(Y)), 12)
-  X = int(Sum[3:6]+Sum[6:9])
-  return X
+def __fixSeedSize(seed : int, l : int) -> str:
+  s = str(seed);
+  if len(s) >= l: 
+    return s[:l]
+  else:
+    return s.rjust(l,'0')
 
-print(Rand(int(input("Seed"))))
-for i in range(0,14):
-  print(Rand())
+ 
+def gen():
+  while True:
+    global __seed
+    X = __fixSeedSize(__seed, 6)
+    Y = (X)[3:6] + (X)[:3];
+    S = __fixSeedSize((int(X) * int(Y)), 12)
+    __seed = int(S[3:9])
+    yield  __seed
+
+seed = input("Enter seed: ")
+initGen(seed)
+rand = gen()
+for i in range(1,16):
+  print(rand.__next__())
