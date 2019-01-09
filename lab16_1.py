5ffbba87541e822c44f9881610c0088f7b69376d
Nadiia <sendahovm@gmail.com>
2019-01-10 00:11:09 +0200

    Update lab16_1.py

--- lab16_1.py
+++ lab16_1.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 html = urlopen("http://www.codeabbey.com/index/user_ranking")
 body_pattern = re.compile(
     r"<table class=\"table table-striped table-bordered full-width ranking-table\">([\s\S]*)</table>")
+a_pattern = re.compile(r'<a href="[\s\S]*?>([\s\S]*?)</a>')
 html_data = html.read().decode('utf-8')
 
 body_text = re.findall(body_pattern, html_data)
@@ -17,6 +18,7 @@ with open('list1.csv', 'w') as csvfile:
         tds = re.findall(re.compile(r'<td>([\S\s\n]*?)</td>'), tr)
         if not tds:
             continue
-        data = [tds[0], tds[3],
-                re.findall(re.compile(r'<span class="rank rank\d+">([\S\s\n]*?)</span>'), tds[4])[0], tds[5].replace(",","")]
+        tds[2] = re.findall(a_pattern, tds[2])[0]
+        data = [tds[0], tds[2], tds[3],
+                re.findall(re.compile(r'<span class="rank rank\d+">([\S\s\n]*?)</span>'), tds[4])[0], tds[5].replace(",",""), tds[7]]
         filewriter.writerow(data)
